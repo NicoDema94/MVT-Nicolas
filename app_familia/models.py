af2ee099c6f552b9d6aaa268cc0ea0258ae434da
Nicolas <nicodemarque@hotmail.com>
2022-09-04 15:50:38 -0300

    primer commit

---
+++ app_familia/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Familia(models.Model):
+    nombre = models.CharField(max_length=128)
+    age = models.IntegerField()
+    nacimiento = models.DateField()
+    pass
+
